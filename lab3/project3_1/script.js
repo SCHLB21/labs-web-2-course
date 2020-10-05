ad129ad99b72f6176006e5394f9c8b9ff0d923c8
Dmitriy <dmitryweazy@gmail.com>
2020-10-05 18:17:17 +0300

    lab 3 created

--- lab3/project3_1/script.js
+++ lab3/project3_1/script.js
@@ -2,11 +2,8 @@ checkPal();
 
 function checkPal() {
     palindrome = prompt('Введите фразу или слово');
-    palindrome = palindrome.split('');
-    palindrome = palindrome.filter(item => item != ' ');
-    palindrome = palindrome.map(arg => { return arg.toLowerCase() });
+    palindrome = palindrome.split('').filter(item => item != ' ').map(arg => { return arg.toLowerCase() });
     R_palindrome = palindrome.concat().reverse()
-
     if (palindrome.join('') == R_palindrome.join('')) {
         alert('Это палиндром');
     } else {
