4320ed9105568b4f631eab5dcba622ac49af3468
Dmitriy <dmitryweazy@gmail.com>
2020-12-03 17:32:40 +0300

    Лабораторная 11

---
+++ lab11/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+export interface Indicator {
+  status: boolean;
+  show: boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  indicators: Indicator[] = [
+    { status: Math.random() >= 0.5, show: true },
+    { status: Math.random() >= 0.5, show: true },
+    { status: Math.random() >= 0.5, show: true },
+    { status: Math.random() >= 0.5, show: true },
+    { status: Math.random() >= 0.5, show: true },
+    { status: Math.random() >= 0.5, show: true },
+    { status: Math.random() >= 0.5, show: true },
+    { status: Math.random() >= 0.5, show: true },
+    { status: Math.random() >= 0.5, show: true },
+    { status: Math.random() >= 0.5, show: true },
+  ];
+}
